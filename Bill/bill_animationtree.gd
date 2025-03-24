b9125d5d53ec0d9897e43de3fcf9b42ce6bb2f8b
gunzblazincourier <gunzblazinAKM@proton.me>
2025-03-24 11:16:50 -0600

    Add very basic and partially functional state transitions

--- Bill/bill_animationtree.gd
+++ Bill/bill_animationtree.gd
@@ -3,32 +3,51 @@ extends CharacterBody2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 var last_facing_direction: float
-#var run_direction: float = Input.get_axis("left", "right")
+var state: int
+
 
 func _process(_delta: float) -> void:
-	#var look_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var look_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	var run_direction: float = Input.get_axis("left", "right")
-	#var last_facing_direction: float
 	
 	if run_direction != 0:
 		last_facing_direction = run_direction
+	#else:
+		#print("AAAAAAAAA")
+	#print(last_facing_direction)
+	
+	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
+	animation_tree.set("parameters/Run/blend_position", look_direction)
+	animation_tree.set("parameters/Jump/blend_position", last_facing_direction)
+	#print(look_direction)
+	
+	print(is_on_floor)
+	if not is_on_floor():
+		state = 1
+		#print(state)
+
+
+func _physics_process(delta: float) -> void:
+	#var look_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	var run_direction: float = Input.get_axis("left", "right")
+	var is_jumping: bool = false
+	
+	if not is_on_floor():
+		velocity.y += 666 * delta
 	else:
-		print("AAAAAAAAA")
+		is_jumping = false
 	
-	var idle: bool
-	var look_up: bool
-	var crouch: bool
+	# Jump code
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		is_jumping = true
+		velocity.y = -300
 	
-	animation_tree.set("parameters/conditions/idle", idle)
-	animation_tree.set("parameters/conditions/crouch", crouch)
-	animation_tree.set("parameters/conditions/look_up", look_up)
+	# Idle if no run_direction, else player runs
+	if run_direction == 0:
+		if is_jumping == false:
+			velocity.x = 0
+	else:
+		velocity.x = run_direction * 100
 	
-	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
-	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
-	print(last_facing_direction)
-
-
-func _physics_process(_delta: float) -> void:
 	var mas: bool = move_and_slide()
 	print(mas)
