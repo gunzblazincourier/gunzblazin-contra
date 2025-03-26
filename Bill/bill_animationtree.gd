6ac0bd3110a73019c44f5e16ad4cad7cf08ec9d1
gunzblazincourier <gunzblazinAKM@proton.me>
2025-03-26 09:46:03 -0600

    Add partial basic implementation of player code using enum of states

--- Bill/bill_animationtree.gd
+++ Bill/bill_animationtree.gd
@@ -2,8 +2,14 @@ extends CharacterBody2D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-var last_facing_direction: float = 1
-var is_jumping: bool = false
+#var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+#var state_machine_state: StringName
+
+enum states {IDLE, RUN, JUMP}
+var current_state: states
+
+var last_facing_direction: float
+var is_jumping: bool
 
 const RUN_SPEED: int = 100
 const JUMP_SPEED: int = -300
@@ -11,9 +17,26 @@ const GRAVITY: int = 666
 
 
 func _ready() -> void:
+	#Engine.time_scale = 0.2
 	animation_tree.active = true
+	
+	current_state = states.IDLE
+	last_facing_direction = 1
+	is_jumping = false
+
 
 func _process(_delta: float) -> void:
+	#var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+	#var state_machine_state: StringName = state_machine.get_current_node()
+	#
+	#match state_machine_state:
+		#"Idle":
+			#current_state = states.IDLE
+		#"Run":
+			#current_state = states.RUN
+		#"Jump":
+			#current_state = states.JUMP
+	
 	var look_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	var run_direction: float = Input.get_axis("left", "right")
 	
@@ -30,25 +53,57 @@ func _process(_delta: float) -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	#var look_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	var run_direction: float = Input.get_axis("left", "right")
+	
+	var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+	var state_machine_state: StringName = state_machine.get_current_node()
+	
+	match state_machine_state:
+		"Idle":
+			current_state = states.IDLE
+		"Run":
+			current_state = states.RUN
+		"Jump":
+			current_state = states.JUMP
 	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	else:
 		is_jumping = false
 	
-	# Jump code
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		is_jumping = true
-		velocity.y = JUMP_SPEED
+	print(Input.is_action_pressed("shoot"))
+	#print(current_state)
+	match current_state:
+		states.IDLE:
+			velocity.x = run_direction * RUN_SPEED
+			if run_direction == 0:
+				if is_jumping == false:
+					velocity.x = 0
+		states.RUN:
+			velocity.x = run_direction * RUN_SPEED
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				is_jumping = true
+				velocity.y = JUMP_SPEED
+		states.JUMP:
+			velocity.x = run_direction * RUN_SPEED
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				is_jumping = true
+				velocity.y = JUMP_SPEED
+				if run_direction == 0:
+					if is_jumping == false:
+						velocity.x = 0
 	
-	# Idle if no run_direction, else player runs
-	if run_direction == 0:
-		if is_jumping == false:
-			velocity.x = 0
-	else:
-		velocity.x = run_direction * RUN_SPEED
+	## Jump code
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+		#is_jumping = true
+		#velocity.y = JUMP_SPEED
+	#
+	## Idle if no run_direction, else player runs
+	#if run_direction == 0:
+		#if is_jumping == false:
+			#velocity.x = 0
+	#else:
+		#velocity.x = run_direction * RUN_SPEED
 	
 	var mas: bool = move_and_slide()
 	print(mas)
