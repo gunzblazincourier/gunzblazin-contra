683b187e9eb30a44df30593013168db067a070b6
gunzblazincourier <gunzblazinAKM@proton.me>
2025-03-28 08:14:13 -0600

    Add code to fire bullet in shoot states

--- Bill/bill_animationtree.gd
+++ Bill/bill_animationtree.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var shoot_timer: Timer = $ShootTimer
+@onready var muzzle: Marker2D = $Muzzle
 
-enum states {IDLE, LOOK_UP, CROUCH, RUN, JUMP}
+enum states {IDLE, LOOK_UP, CROUCH, RUN, JUMP, SHOOT_IDLE, SHOOT_LOOK_UP, \
+		SHOOT_CROUCH, SHOOT_RUN, SHOOT_JUMP, SHOOT_JUMP_UP, SHOOT_JUMP_DOWN}
 var current_state: states
+
+enum bullet_id {R, M, S, F, L}
+var current_bullet_id: bullet_id
 
 # Direction that sprite is currently facing; needed for blend position of states like IDLE
 var sprite_direction: float
@@ -20,6 +26,7 @@ func _ready() -> void:
 	animation_tree.active = true
 	current_state = states.JUMP
 	sprite_direction = 1
+	current_bullet_id = bullet_id.R
 
 
 func _process(_delta: float) -> void:
@@ -32,14 +39,20 @@ func _process(_delta: float) -> void:
 	
 	# Will set the advance condition value of respective states
 	# Set after storing in variable instead of setting directly because of long statements
-	var idle: bool = is_on_floor() and look_direction.x == 0 and look_direction.y == 0
-	var look_up: bool = is_on_floor() and look_direction.x == 0 and look_direction.y == -1
-	var crouch: bool = is_on_floor() and look_direction.x == 0 and look_direction.y == 1
+	var idle: bool = is_on_floor() and look_direction == Vector2(0, 0)
+	var look_up: bool = is_on_floor() and look_direction == Vector2(0, -1)
+	var crouch: bool = is_on_floor() and look_direction == Vector2(0, 1)
+	var shoot_jump: bool = not is_on_floor() and look_direction.y == 0
+	var shoot_jump_up: bool = not is_on_floor() and look_direction.y == -1
+	var shoot_jump_down: bool = not is_on_floor() and look_direction.y == 1
 	
 	# Sets advance condition true or false for each state
 	animation_tree.set("parameters/conditions/idle", idle)
 	animation_tree.set("parameters/conditions/look_up", look_up)
 	animation_tree.set("parameters/conditions/crouch", crouch)
+	animation_tree.set("parameters/conditions/shoot_jump", shoot_jump)
+	animation_tree.set("parameters/conditions/shoot_jump_up", shoot_jump_up)
+	animation_tree.set("parameters/conditions/shoot_jump_down", shoot_jump_down)
 	
 	# Sets blend position (direction) for each state so appropriate animation can be displayed
 	animation_tree.set("parameters/Idle/blend_position", sprite_direction)
@@ -47,6 +60,17 @@ func _process(_delta: float) -> void:
 	animation_tree.set("parameters/Crouch/blend_position", sprite_direction)
 	animation_tree.set("parameters/Run/blend_position", look_direction)
 	animation_tree.set("parameters/Jump/blend_position", sprite_direction)
+	
+	animation_tree.set("parameters/ShootIdle/blend_position", sprite_direction)
+	animation_tree.set("parameters/ShootLookUp/blend_position", sprite_direction)
+	animation_tree.set("parameters/ShootCrouch/blend_position", sprite_direction)
+	animation_tree.set("parameters/ShootRun/blend_position", look_direction)
+	animation_tree.set("parameters/ShootJump/blend_position", look_direction)
+	animation_tree.set("parameters/ShootJumpUp/blend_position", sprite_direction)
+	animation_tree.set("parameters/ShootJumpDown/blend_position", sprite_direction)
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot_timer.start()
 	
 	# Basically gets AnimationNodeStateMachine from AnimationTree
 	var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
@@ -66,6 +90,30 @@ func _process(_delta: float) -> void:
 			current_state = states.RUN
 		"Jump":
 			current_state = states.JUMP
+		"ShootIdle":
+			current_state = states.SHOOT_IDLE
+		"ShootLookUp":
+			current_state = states.SHOOT_LOOK_UP
+		"ShootCrouch":
+			current_state = states.SHOOT_CROUCH
+		"ShootRun":
+			current_state = states.SHOOT_RUN
+		"ShootJump":
+			current_state = states.SHOOT_JUMP
+		"ShootJumpUp":
+			current_state = states.SHOOT_JUMP_UP
+		"ShootJumpDown":
+			current_state = states.SHOOT_JUMP_DOWN
+	
+	match current_state:
+		states.SHOOT_IDLE, states.SHOOT_LOOK_UP, states.SHOOT_CROUCH, states.SHOOT_RUN, \
+				states.SHOOT_JUMP, states.SHOOT_JUMP_UP, states.SHOOT_JUMP_DOWN:
+			var bullet_r_path: PackedScene = load("res://Bullet/bullet_r.tscn")
+			var bullet_r: Area2D = bullet_r_path.instantiate()
+			
+			owner.add_child(bullet_r)
+			bullet_r.position = muzzle.global_position
+			bullet_r.rotation = muzzle.global_rotation
 
 
 func _physics_process(delta: float) -> void:
