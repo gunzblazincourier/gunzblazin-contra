b29ec7939cd308efa6b6267174c950c747e9bdc5
gunzblazincourier <gunzblazinAKM@proton.me>
2025-04-24 11:03:00 -0600

    Make TurretSoldier respond to mouse cursor based on a timer timeout

--- Enemies/turret_soldier.gd
+++ Enemies/turret_soldier.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+#func _process(_delta: float) -> void:
+	#var angle_with_mouse: float = global_position.angle_to_point(get_global_mouse_position())
+	#print(angle_with_mouse)
+	#
+	#if angle_with_mouse < -0.79 and angle_with_mouse > -2.36:
+		#animated_sprite_2d.play("shoot_up")
+	#elif angle_with_mouse > 0.79 and angle_with_mouse < 2.36:
+		#animated_sprite_2d.play("shoot_down")
+	#else:
+		#animated_sprite_2d.play("shoot_straight")
+
+
+func _on_timer_timeout() -> void:
 	var angle_with_mouse: float = global_position.angle_to_point(get_global_mouse_position())
 	print(angle_with_mouse)
 	
@@ -14,3 +27,4 @@ func _process(_delta: float) -> void:
 		animated_sprite_2d.play("shoot_down")
 	else:
 		animated_sprite_2d.play("shoot_straight")
+	timer.start()
