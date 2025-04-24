4a36dc53f82919e0c959523305507ff271a84f79
gunzblazincourier <gunzblazinAKM@proton.me>
2025-04-24 08:28:53 -0600

    Fixed TurretSoldier shoot_up sprites' positions to match other shoot animations of TUrretSoldier

--- main.gd
+++ main.gd
@@ -1,9 +1,13 @@
-extends Area2D
+extends Node
+
+@onready var bill: CharacterBody2D = $Bill
+@onready var turret_soldier: Area2D = $TurretSoldier
+
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
