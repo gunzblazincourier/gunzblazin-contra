2d36e96b044e44871641edfbaedfa0ef173262b5
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-01 17:20:30 -0600

    feat: add post-death explosion to TurretSoldier

--- Enemies/turret_soldier.gd
+++ Enemies/turret_soldier.gd
@@ -10,6 +10,7 @@ const GRAVITY: int = 555			## Custom gravity for the enemy
 const DEATH_SPEED_X: int = 70		## Horizontal speed during death
 var death_speed_y: float			## Vertical speed during death
 var is_dead: bool					## Whether soldier got hit
+var is_exploding: bool				## Whether soldier is explosing post-death
 
 ## Enemy animations
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -23,10 +24,45 @@ var is_dead: bool					## Whether soldier got hit
 ## Muzzle of gun
 @onready var muzzle: Marker2D = $Muzzle
 
+## Death explosion sound
+@onready var death_explosion_sfx: AudioStreamPlayer2D = $DeathExplosionSFX
+
 
 func _ready() -> void:
 	death_speed_y = -150
 	is_dead = false
+	is_exploding = false
+
+
+## Handles death explosion
+func _process(_delta: float) -> void:
+	if is_dead == true:
+		animated_sprite_2d.play("death")
+		collision_shape_2d.disabled = true
+		timer.stop()
+	
+	if is_exploding == true:
+		animated_sprite_2d.play("explode")
+		death_explosion_sfx.play()
+		is_exploding = false
+
+
+## Handles movement during death
+func _physics_process(delta: float) -> void:
+	if is_dead == true:
+		# Decides which direction would the soldier travel when hit by player
+		if animated_sprite_2d.flip_h == true:
+			position.x += DEATH_SPEED_X * delta
+		else:
+			position.x += DEATH_SPEED_X * delta
+		
+		# Vertical movement with gravity
+		position.y += death_speed_y * delta
+		death_speed_y += GRAVITY * delta
+		
+		if death_speed_y > 0:
+			is_exploding = true
+			is_dead = false
 
 
 ## Soldier aims and fire at the player each time the repeating timer timeouts
@@ -78,30 +114,14 @@ func _on_timer_timeout() -> void:
 	owner.add_child(bullet)
 	bullet.position = muzzle.global_position
 	bullet.rotation = muzzle.global_rotation
-	
-	if is_dead == true:
-		animated_sprite_2d.play("death")
-		collision_shape_2d.disabled = true
-
-
-## Handles movement during death
-func _physics_process(delta: float) -> void:
-	if is_dead == true:
-		# Decides which direction would the soldier travel when hit by player
-		if animated_sprite_2d.flip_h == true:
-			position.x += DEATH_SPEED_X * delta
-		else:
-			position.x += DEATH_SPEED_X * delta
-		
-		# Vertical movement with gravity
-		position.y += death_speed_y * delta
-		death_speed_y += GRAVITY * delta
-		
-		if death_speed_y > 0:
-			queue_free()
 
 
 ## Enemy dies when bullet hits it
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
 		is_dead = true
+
+
+## Remove soldier when explosion sound is finished playing
+func _on_death_explosion_sfx_finished() -> void:
+	queue_free()
