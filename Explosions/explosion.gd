03005c885636cc29a43bdaf11dced57719e41211
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-01 02:26:19 -0600

    Animate explosion based on toggle

---
+++ Explosions/explosion.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var play: bool
+var exploded: bool
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+	exploded = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if exploded == false:
+		if play == true:
+			animation_player.play("explode")
