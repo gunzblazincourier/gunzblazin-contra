7d39d427b9fcbfe6ec02f9d23a2f9470f21beb7a
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-01 15:31:29 -0600

    feat: disables laser when outside screen and reenables when respawned (not removed since only one instance of laser)

--- Bullet/bullet_l.gd
+++ Bullet/bullet_l.gd
@@ -10,3 +10,19 @@ const BULLET_SPEED: int = 400
 ## Bullet travels in linear direction, without changing rotation or direction
 func _physics_process(delta: float) -> void:
 	position += transform.x * BULLET_SPEED * delta
+
+
+# Cannot remove laser since only one instance is made to respawn laser
+# instead of making another one, so it is just disabled until visible again
+## Disables laser once outside screen
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	print("gone")
+	monitoring = false
+	monitorable = false
+
+
+## Reenables laser when in view
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	print("back")
+	monitoring = true
+	monitorable = true
