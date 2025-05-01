9494625bd8f4b6fb35741e77b96fb2283ce03713
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-01 15:24:16 -0600

    feat: remove Machinegun bullet once out-of-view

--- Bullet/bullet_m.gd
+++ Bullet/bullet_m.gd
@@ -16,3 +16,7 @@ func _physics_process(delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
 		queue_free()
+
+## Removes bullet once outside screen
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
