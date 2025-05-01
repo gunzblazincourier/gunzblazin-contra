ddd07749bd824d50d4dd01af926d2d1e692fa93b
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-01 16:26:19 -0600

    docs: document TurretSoldier script and improve naming; insert a TurretSoldierin level for test

--- Enemies/turret_soldier.gd
+++ Enemies/turret_soldier.gd
@@ -1,14 +1,24 @@
 extends Area2D
+## Soldier that remains stationary and shooting at the player at fixed
+## intervals
+##
+## Handles tracking the player and shooting at it, and death of enemy
 
+## Enemy animations
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+## Basically rate of fire
 @onready var timer: Timer = $Timer
-@onready var marker_2d: Marker2D = $Marker2D
 
+## Muzzle of gun
+@onready var muzzle: Marker2D = $Muzzle
 
+## Soldier aims and fire at the player each time the repeating timer timeouts
 func _on_timer_timeout() -> void:
+	## Angle between the soldier and player relative to X-axis
 	var angle_with_player: float = global_position.angle_to_point(Global.player_global_position)
-	#print(angle_with_player)
 	
+	# Above angle decides animation
 	if angle_with_player < -PI/4 and angle_with_player > -3*(PI/4):
 		animated_sprite_2d.play("shoot_up")
 	elif angle_with_player > PI/4 and angle_with_player < 3*(PI*4):
@@ -16,38 +26,39 @@ func _on_timer_timeout() -> void:
 	else:
 		animated_sprite_2d.play("shoot_straight")
 	
+	# Soldier faces left or right based on the angle with player
 	if angle_with_player > -PI/2 and angle_with_player < PI/2:
 		animated_sprite_2d.flip_h = false
 	else:
 		animated_sprite_2d.flip_h = true
 	
+	# Initializes muzzle position and rotation based on animation
+	# Rotation value is clamped between two angles
 	if animated_sprite_2d.animation == "shoot_up":
 		if animated_sprite_2d.flip_h == false:
-			marker_2d.position = Vector2(9, -21)
-			marker_2d.rotation = clamp(angle_with_player, -PI/2, -PI/8)
+			muzzle.position = Vector2(9, -21)
+			muzzle.rotation = clamp(angle_with_player, -PI/2, -PI/8)
 		else:
-			marker_2d.position = Vector2(-9, -21)
-			marker_2d.rotation = clamp(angle_with_player, -7*(PI/8), -PI/2)
+			muzzle.position = Vector2(-9, -21)
+			muzzle.rotation = clamp(angle_with_player, -7*(PI/8), -PI/2)
 	elif animated_sprite_2d.animation == "shoot_down":
 		if animated_sprite_2d.flip_h == false:
-			marker_2d.position = Vector2(14, 2)
-			marker_2d.rotation = clamp(angle_with_player, PI/8, PI/2)
+			muzzle.position = Vector2(14, 2)
+			muzzle.rotation = clamp(angle_with_player, PI/8, PI/2)
 		else:
-			marker_2d.position = Vector2(-14, 2)
-			marker_2d.rotation = clamp(angle_with_player, PI/2, 7*(PI/8))
+			muzzle.position = Vector2(-14, 2)
+			muzzle.rotation = clamp(angle_with_player, PI/2, 7*(PI/8))
 	else:
 		if animated_sprite_2d.flip_h == false:
-			marker_2d.position = Vector2(14, -11)
-			marker_2d.rotation = 0
+			muzzle.position = Vector2(14, -11)
+			muzzle.rotation = 0
 		else:
-			marker_2d.position = Vector2(-14, -11)
-			marker_2d.rotation = PI
+			muzzle.position = Vector2(-14, -11)
+			muzzle.rotation = PI
 	
+	# Spawn bullet
 	var bullet_path: PackedScene = load("res://Bullet/bullet_ts.tscn")
 	var bullet: Area2D = bullet_path.instantiate()
 	owner.add_child(bullet)
-	bullet.position = marker_2d.global_position
-	bullet.rotation = marker_2d.global_rotation
-	
-	#print(marker_2d.rotation)
-	timer.start()
+	bullet.position = muzzle.global_position
+	bullet.rotation = muzzle.global_rotation
