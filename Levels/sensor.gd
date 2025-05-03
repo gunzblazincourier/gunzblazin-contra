2c37bf79ce9b064d42b94ee408e2a88c8a5a8389
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-03 14:22:32 -0600

    Add explosion trigger with sound and then removal of sensor

--- Levels/sensor.gd
+++ Levels/sensor.gd
@@ -4,17 +4,26 @@ extends Area2D
 ##
 ## Handles its animation, destruction and spawning of the pickup
 
+## Whether sensor has been destoryed
+var destroyed: bool
+
 ## Animated sprite for the sensor, containing the default animation and the
 ## explosion
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+## Sound for when sensor is destroyed
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 func _ready() -> void:
-	pass # Replace with function body.
+	destroyed = false
 
 
 func _process(_delta: float) -> void:
-	pass
+	if destroyed == true:
+		animated_sprite_2d.play("explode")
+		audio_stream_player_2d.play()
+		destroyed = false
 
 
 ## Default animation starts when FULLY in the camera
@@ -27,7 +36,14 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 
+## Sensor is destroyed after shot by player
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet") and animated_sprite_2d.frame > 4 and \
 			animated_sprite_2d.frame < 14:
-		animated_sprite_2d.play("explode")
+		destroyed = true
+
+
+## Removes sensor after explosion (based on the sound instead of animation since
+## sound is longer)
+func _on_audio_stream_player_2d_finished() -> void:
+	queue_free()
