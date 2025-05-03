4adf50c2e464d0e365685cc96a381696d37a2f9e
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-03 14:09:43 -0600

    Initiate explosion of sensor when shot by player

--- Levels/sensor.gd
+++ Levels/sensor.gd
@@ -25,3 +25,8 @@ func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 ## Removes sensor once off-screen
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		animated_sprite_2d.play("explode")
