478ccb2e65dfe188d559d33f25dc9453042a0054
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-03 14:01:09 -0600

    docs: create documentation for sensor script

--- Levels/sensor.gd
+++ Levels/sensor.gd
@@ -1,17 +1,22 @@
 extends Area2D
+## Boxes which open and close. When opened, it can be shot to give a weapon
+## pickup
+##
+## Handles its animation, destruction and spawning of the pickup
 
+## Animated sprite for the sensor, containing the default animation and the
+## explosion
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 
+## Default animation starts when FULLY in the camera
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	animated_sprite_2d.play("default")
