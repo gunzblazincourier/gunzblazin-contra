b7200e0012afdef47739a7fa9f69566ae80db1bb
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-04 11:14:54 -0600

    Script turret activation/decativation based on visibility on-screen; delete one VisibilityOnScreenNotifier since not needed and adjust other's size

--- Enemies/turret.gd
+++ Enemies/turret.gd
@@ -2,4 +2,11 @@ extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var visible_on_screen_notifier_2d_2: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D2
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	animated_sprite_2d.play("activate")
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	animated_sprite_2d.play_backwards("activate")
