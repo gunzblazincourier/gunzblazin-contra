ec41b7539993d26a68223b1a8f101762d51488ef
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-03 20:16:03 -0600

    Script pickup movement and insert one in level for testing

--- Bullet/pickup.gd
+++ Bullet/pickup.gd
@@ -1,11 +1,27 @@
 extends Area2D
+## Weapon pickups
+##
+## Controls its speed, trajectory and the action of 'picking' by the player
+
+## Fixed horizontal pickup speed
+const PICKUP_SPEED_X: int = 70
+
+## Custom gravity
+const GRAVITY: int = 555
+
+## Non-constant vertical speed
+var pickup_speed_y: float
+
+## RayCast to detect landing
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pickup_speed_y = -150
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+## Pickup travels in an arc with constant horizontal speed
+func _physics_process(delta: float) -> void:
+	position.x += PICKUP_SPEED_X * delta
+	position.y += pickup_speed_y * delta
+	pickup_speed_y += GRAVITY * delta
