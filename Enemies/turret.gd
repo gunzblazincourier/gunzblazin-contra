0a64690cc359165e4ccc0db17bdc51dbd1838f90
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-04 16:44:12 -0600

    Play shoot animation after activation animation is finished

--- Enemies/turret.gd
+++ Enemies/turret.gd
@@ -11,3 +11,7 @@ func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 
 func _on_visible_on_screen_notifier_2d_2_screen_exited() -> void:
 	animated_sprite_2d.play_backwards("activate")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	animated_sprite_2d.play("shoot")
