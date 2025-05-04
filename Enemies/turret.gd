2a221a80ce7dedb59b8c8f3ae0a9c77ba6e875d8
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-04 11:08:31 -0600

    Adjust turret VisibleOnScreenNotifier sizes

--- Enemies/turret.gd
+++ Enemies/turret.gd
@@ -1,3 +1,5 @@
 extends Area2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var visible_on_screen_notifier_2d_2: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D2
