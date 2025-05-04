65667962ed94245ec8e9fa9e8a74a45c9123d3e7
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-03 20:19:10 -0600

    Adjust pickup travel speed

--- Bullet/pickup.gd
+++ Bullet/pickup.gd
@@ -7,7 +7,7 @@ extends Area2D
 const PICKUP_SPEED_X: int = 70
 
 ## Custom gravity
-const GRAVITY: int = 555
+const GRAVITY: int = 350
 
 ## Non-constant vertical speed
 var pickup_speed_y: float
@@ -17,7 +17,7 @@ var pickup_speed_y: float
 
 
 func _ready() -> void:
-	pickup_speed_y = -150
+	pickup_speed_y = -175
 
 
 ## Pickup travels in an arc with constant horizontal speed
