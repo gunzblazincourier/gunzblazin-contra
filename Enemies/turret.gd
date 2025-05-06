59536f60faab9e0c60204fdcf2ba2f6cda54226b
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-06 11:57:58 -0600

    Add variable for angle exactly mirroring the turret muzzle's rotation

--- Enemies/turret.gd
+++ Enemies/turret.gd
@@ -12,6 +12,7 @@ func _process(_delta: float) -> void:
 	var angle_with_player: float = global_position.angle_to_point(Global.player_global_position)
 	var angle_min: float
 	var angle_max: float
+	var mirror: float
 	if animated_sprite_2d.animation == "shoot":
 		if animated_sprite_2d.frame == 0:
 			#if angle_with_player < -11*(PI/12) and angle_with_player > 11*(PI/12):
@@ -20,62 +21,75 @@ func _process(_delta: float) -> void:
 			muzzle.rotation = PI
 			angle_max = -11*(PI/12)
 			angle_min = 11*(PI/12)
+			mirror = 0
 		elif animated_sprite_2d.frame == 1:
 			muzzle.position = Vector2(-13, 6)
 			muzzle.rotation = 5*(PI/6)
 			angle_max = 11*(PI/12)
 			angle_min = 9*(PI/12)
+			mirror = -PI/6
 		elif animated_sprite_2d.frame == 2:
 			muzzle.position = Vector2(-7, 12)
 			muzzle.rotation = 4*(PI/6)
 			angle_max = 9*(PI/12)
 			angle_min = 7*(PI/12)
+			mirror = -2*(PI/6)
 		elif animated_sprite_2d.frame == 3:
 			muzzle.position = Vector2(0, 13)
 			muzzle.rotation = 3*(PI/6)
 			angle_max = 7*(PI/12)
 			angle_min = 5*(PI/12)
+			mirror = -3*(PI/6)
 		elif animated_sprite_2d.frame == 4:
 			muzzle.position = Vector2(7, 12)
 			muzzle.rotation = 2*(PI/6)
 			angle_max = 5*(PI/12)
 			angle_min = 3*(PI/12)
+			mirror = -4*(PI/6)
 		elif animated_sprite_2d.frame == 5:
 			muzzle.position = Vector2(12, 7)
 			muzzle.rotation = PI/6
 			angle_max = 3*(PI/12)
 			angle_min = 1*(PI/12)
+			mirror = -5*(PI/6)
 		elif animated_sprite_2d.frame == 6:
 			muzzle.position = Vector2(13, 0)
 			muzzle.rotation = 0
 			angle_max = 1*(PI/12)
 			angle_min = -1*(PI/12)
+			mirror = PI
 		elif animated_sprite_2d.frame == 7:
 			muzzle.position = Vector2(12, -7)
 			muzzle.rotation = -PI/6
 			angle_max = -1*(PI/12)
 			angle_min = -3*(PI/12)
+			mirror = PI/6
 		elif animated_sprite_2d.frame == 8:
 			muzzle.position = Vector2(7, -12)
 			muzzle.rotation = -2*(PI/6)
 			angle_max = -3*(PI/12)
 			angle_min = -5*(PI/12)
+			mirror = 2*(PI/6)
 		elif animated_sprite_2d.frame == 9:
 			muzzle.position = Vector2(-1, -14)
 			muzzle.rotation = -3*(PI/6)
 			angle_max = -5*(PI/12)
 			angle_min = -7*(PI/12)
+			mirror = 3*(PI/6)
 		elif animated_sprite_2d.frame == 10:
 			muzzle.position = Vector2(-8, -13)
 			muzzle.rotation = -4*(PI/6)
 			angle_max = -7*(PI/12)
 			angle_min = -9*(PI/12)
+			mirror = 4*(PI/6)
 		elif animated_sprite_2d.frame == 11:
 			muzzle.position = Vector2(-12, -7)
 			muzzle.rotation = -5*(PI/6)
 			angle_max = -9*(PI/12)
 			angle_min = -11*(PI/12)
+			mirror = 5*(PI/6)
 		
+		print(mirror)
 		if shoot_timer.is_stopped():
 			if animated_sprite_2d.frame != 0:
 				if angle_with_player < angle_min:
@@ -103,7 +117,7 @@ func _process(_delta: float) -> void:
 					bullet.position = muzzle.global_position
 					bullet.rotation = muzzle.global_rotation
 					animated_sprite_2d.pause()
-		print(angle_with_player)
+		#print(angle_with_player)
 		#print(angle_min)
 		#print(angle_with_player < angle_min)
 
