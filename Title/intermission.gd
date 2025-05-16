ce2937dd0c00d6ecc8ee6267ffe00af1b29bf83c
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-16 15:49:36 -0600

    Code flashing scores on intermission screen

--- Title/intermission.gd
+++ Title/intermission.gd
@@ -1,13 +1,13 @@
 extends Control
 
+@onready var score: Label = $Score
+@onready var high_score: Label = $HighScore
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+func _on_label_flashing_timer_timeout() -> void:
+	if score.visible == true:
+		score.visible = false
+		high_score.visible = false
+	else:
+		score.visible = true
+		high_score.visible = true
