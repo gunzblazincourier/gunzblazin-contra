35fd65cde9e7e683ae8814ccebfd161dc9bbe241
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-17 11:05:24 -0600

    Update level1's scene name in intermission's script

--- Title/intermission.gd
+++ Title/intermission.gd
@@ -16,6 +16,6 @@ func _on_label_flashing_timer_timeout() -> void:
 func _on_screen_timer_timeout() -> void:
 	# Stored in variable and printed to prevent 'return value
 	# discarded' error
-	var er: Error = get_tree().change_scene_to_file("res://main.tscn")
+	var er: Error = get_tree().change_scene_to_file("res://Levels/level1.tscn")
 	print(er)
 	print("BEGIN")
