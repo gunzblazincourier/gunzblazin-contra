da1ebd70738ecff00feb12a6eb7d9354c8948648
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-17 11:02:06 -0600

    Proceed to level from intermission screen after screen timeouts

--- Title/intermission.gd
+++ Title/intermission.gd
@@ -14,4 +14,8 @@ func _on_label_flashing_timer_timeout() -> void:
 
 
 func _on_screen_timer_timeout() -> void:
+	# Stored in variable and printed to prevent 'return value
+	# discarded' error
+	var er: Error = get_tree().change_scene_to_file("res://main.tscn")
+	print(er)
 	print("BEGIN")
