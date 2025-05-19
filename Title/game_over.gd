1b2927dd8a40bca6bd25ead5fbbbcfc633f337de
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-19 11:23:12 -0600

    Document game over screen's script

--- Title/game_over.gd
+++ Title/game_over.gd
@@ -1,14 +1,25 @@
 extends Control
+## Game over screen
+##
+## Shown when player loses all lives. Option to either end game or start level
+## again
 
+## Current score of player
 @onready var score: Label = $Score
+
+## High score which is updated when player score more than this score
 @onready var high_score: Label = $HighScore
+
+## Cursor to select 'Continue' or 'End' option
 @onready var cursor: TextureRect = $Cursor
 
 
+## Update score when screen is displayed
 func _ready() -> void:
 	score.text = str(Global.score)
 
 
+## Flash scores
 func _on_label_flashing_timer_timeout() -> void:
 	if score.visible == true:
 		score.visible = false
@@ -21,13 +32,17 @@ func _on_label_flashing_timer_timeout() -> void:
 func _process(_delta: float) -> void:
 	var er: Error
 	if Input.is_action_just_pressed("start"):
+		# Perform respective action based on option selected
 		if cursor.position.y == 166:
-			Global.lives = 1
+			Global.lives = Global.DEFAULT_LIVES
 			er = get_tree().change_scene_to_file("res://Title/intermission.tscn")
 		else:
 			er = get_tree().change_scene_to_file("res://Title/title.tscn")
+	# Stored in variable and printed to prevent 'return value
+	# discarded' error
 	print(er)
 	
+	# Allow option selection
 	if Input.is_action_just_pressed("select"):
 		if cursor.position.y == 166:
 			cursor.position.y = 181
