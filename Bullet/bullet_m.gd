e192ca2c97901fe265914b89f4ba2a98a74fab5d
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-22 19:29:37 -0600

    Add bullet impact to Regulargun and Machinegun bullets

--- Bullet/bullet_m.gd
+++ Bullet/bullet_m.gd
@@ -6,18 +6,27 @@ extends Area2D
 ## Fixed bullet speed
 const BULLET_SPEED: int = 400
 
+## Animated sprite
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 ## Bullet travels in linear direction, without changing rotation or direction
 func _physics_process(delta: float) -> void:
-	position += transform.x * BULLET_SPEED * delta
+	if animated_sprite_2d.animation == "default":
+		position += transform.x * BULLET_SPEED * delta
 
 
 ## Removes bullet if it enters specific hitboxes (eg an enemy's)
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy") or area.is_in_group("object"):
-		queue_free()
+		animated_sprite_2d.play("hit")
 
 
 ## Removes bullet once outside screen
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "hit":
+		queue_free()
