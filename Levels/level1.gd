e96224ab48682fefecef7afb93c5ffc39461cb67
gunzblazincourier <gunzblazinAKM@proton.me>
2025-05-31 22:15:25 -0600

    Place Capsules at desired locations in level

--- Levels/level1.gd
+++ Levels/level1.gd
@@ -30,6 +30,7 @@ extends Node
 
 ## Locks player position to center of camera view when player reaches center
 func _process(_delta: float) -> void:
+	print(Global.camera_center_position)
 	if Global.lives >= 3:
 		lives.play("lives_3")
 	elif Global.lives == 2:
